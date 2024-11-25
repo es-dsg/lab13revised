4369593eefb28d01fce823782450d0e9ea291b8b
es-dsg <167735883+es-dsg@users.noreply.github.com>
2024-11-25 11:05:07 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,62 @@
 '''
 DEVELOPER: julia de guzman
-COLLABORATORS: <anyone you worked with>
-DATE: <11/23/2024
+COLLABORATORS: <anyone you worked with or who helped you>
+DATE:11/24/2024
 '''
 
-"""generates lottery tickets, either randomly or by allowing the user to enter their own numbers.
-
-Leave one blank line.  The rest of this docstring should contain an
-overall description of the program.
+"""
+To organize different species that are affected by Climate Change. The most fundamental part of inheritance is to be able to reuse attributes and methods that are share among different groups. For example there are many different mammals species. We are looking at two main species affected by Climate Change, polar bears and snow leopards.
 """
 
-##########################################
-# IMPORTS:
-#   <list modules needed for program and their purpose>
-##########################################
-import random
+##### IMPORTS #####
+from mammal import *
+from polarbear import *
+from snowleopard import *
 
-##########################################
-# FUNCTIONS:
-##########################################
-def randomList():
-    list2=random.sample(range(1,50),5)
-    list2.append(random.randint(1,19))
-    return list2
-
-def customTicket():
-    list1 = [0,0,0,0,0]
-    list2=[]
-    for element in list1:
-        element= int(input("Enter your lottery number between 1 and 49: "))
-        if element in list2:
-            element=input("Invalid input. Enter your lottery number between 1 and 49: ")
-        
-        list2.append(int(element))
-    
-    megaNumber= int(input("Enter your mega number between 1 and 19: "))
-    list2.append(megaNumber)
-    return list2
-        
-
-
-##########################################
-# MAIN PROGRAM:
-##########################################
+##### FUNCTIONS #####
+# create your main definition
 def main():
-    pass
+  mammalObject = Mammal("eukaryota", "Animalia", "chordata", "mammalia")
+  print(mammalObject)
 
-main()
-print("Welcome to Python Lotto!")
-print("You have the option to choose your own lottery numbers or have them randomly selected for you.")
-choose = input("Please enter C for custom or R for random:\n")
-if choose.upper() == "C":
-    list2 = customTicket()
-    print((f"Your lottery ticket is: {list2[0]:02d} {list2[1]:02d} {list2[2]:02d} {list2[3]:02d} {list2[4]:02d} (mega {list2[5]})"))
-elif choose.upper() =="R":
-    list2 = randomList()
-    print((f"Your lottery ticket is: {list2[0]:02d} {list2[1]:02d} {list2[2]:02d} {list2[3]:02d} {list2[4]:02d} (mega {list2[5]})"))
+  polarBearObject = PolarBear("eukaryota", "Animalia", "chordata", "mammalia", "polarbear")
+  print(polarBearObject)
+  
+  snowLeopardObject = SnowLeopard("eukaryota", "Animalia", "chordata", "mammalia", "snowLeopard")
+  print(snowLeopardObject)
+  
+  # leave this part as it is
+  print("Climate change extreme weather events not only affect us economically, air quality health, and the destruction of cities by tornados, wildfires, hurricanes; we never think about how is affecting other species we share this world with.\n")
+  print("Nikhil Advani conducted animal species assessments affected by Climate Change and determined their vulnerability, resilience, and status.\n")
+
+  # create your menu to choose between Polar Bear, Snow Leopard and Exit follow by a submenu to choose between Vulnerability, Resilience, Status, and Exit
+  while (True):
+    choose = input("Choose animal under danger: a) Polar Bear, b)Snow Leopard, c) Exit\n")
+    while (choose == "a"):
+      choose2 = input("Choose animal's: a) Vulnerabiliy b) Resilience c) Status d) Exit\n")
+      if choose2=="a":
+        print(polarBearObject.get_vulnerability())
+      elif choose2=="b":
+        print(polarBearObject.get_resilience())
+      elif choose2 =="c":
+        print(polarBearObject.get_status())
+      elif choose2=="d":
+        break
+    while choose == "b":
+      choose2 = input("Choose animal's: a) Vulnerabiliy b) Resilience c) Status d) Exit\n")
+      if choose2=="a":
+        print(snowLeopardObject.get_vulnerability())
+      elif choose2=="b":
+        print(snowLeopardObject.get_resilience())
+      elif choose2 =="c":
+        print(snowLeopardObject.get_status())
+      elif choose2=="d":
+        break
+    if choose == "c":
+      print("Goodbye!")
+      break
+
+##### MAIN PROGRAM #####    
+if __name__ == '__main__':
+  main()
 
