b280a668dd81812466b8f4045c7324d6df87808f
es-dsg <167735883+es-dsg@users.noreply.github.com>
2024-11-30 12:21:10 -0800

    Update UML.py

--- UML.py
+++ UML.py
@@ -3,26 +3,26 @@
 '''
 Mammal
 --------------------------
-    self.domain : string
-    self.kingdom :string
-    self.phylum :string
-    self.class_type:string
+    + domain : string
+    + kingdom :string
+    + phylum :string
+    + class_type:string
 
 
 
 
 --------------------------
 methods
-__init__(self, domain, kingdom, phylum, class_type)
-__str__(self)
-set_domain(self, domain)
-set_kingdom(self, kingdom)
-set_phylum(self, phylum)
-set_kingdom(self, class_type)
-get_domain(self)
-get_kingdom(self)
-get_phylum(self)
-get_class_type(self)
+- __init__(domain, kingdom, phylum, class_type)
+- __str__()
++ set_domain(domain)
++ set_kingdom(kingdom)
++ set_phylum(phylum)
++ set_kingdom(class_type)
++ get_domain()
++ get_kingdom()
++ get_phylum()
++ get_class_type()
 
 '''
 # The PolarBear subclass that inherits from the Mammal superclass
@@ -30,35 +30,35 @@ get_class_type(self)
 PolarBear
 --------------------------
 data attributes
-    self.name : string
-    self.order : string
-    self.family :string
-    self.genus : string
-    self.species : string
-    self.vulnerability : string
-    self.resilience : string
-    self.status: string
+    + name : string
+    + order : string
+    + family :string
+    + genus : string
+    + species : string
+    + vulnerability : string
+    + resilience : string
+    + status: string
 
 
 
 --------------------------
 methods
-__init__(self, domain, kingdom, phylum, class_type,name)
-__str__(self)
-set_order(self,order)
-set_family(self,family)
-set_genus(self,genus)
-set_species(self,species)
-set_vulnerability(self,vulnerability)
-set_resilience(self,resilience)
-set_status(self,status)
-get_order(self)
-get_family(self)
-get_genus(self)
-get_species(self)
-get_vulnerability(self)
-get_resilience(self)
-get_status(self)
+- __init__(domain, kingdom, phylum, class_type,name)
+- __str__()
++ set_order(order)
++ set_family(family)
++ set_genus(genus)
++ set_species(species)
++ set_vulnerability(vulnerability)
++ set_resilience(resilience)
++ set_status(status)
++ get_order()
++ get_family()
++ get_genus()
++ get_species()
++ get_vulnerability()
++ get_resilience()
++ get_status()
 
 '''
 # The SnowLeopard subclass that inherits from the Mammal superclass
@@ -66,35 +66,35 @@ get_status(self)
 SnowLeopard
 --------------------------
 data attributes
-    self.name : string
-    self.order : string
-    self.family :string
-    self.genus : string
-    self.species : string
-    self.vulnerability : string
-    self.resilience : string
-    self.status: string
+    + name : string
+    + order : string
+    + family :string
+    + genus : string
+    + species : string
+    + vulnerability : string
+    + resilience : string
+    + status: string
 
 
 
 --------------------------
 methods
-__init__(self, domain, kingdom, phylum, class_type,name)
-__str__(self)
-set_order(self,order)
-set_family(self,family)
-set_genus(self,genus)
-set_species(self,species)
-set_vulnerability(self,vulnerability)
-set_resilience(self,resilience)
-set_status(self,status)
-get_order(self)
-get_family(self)
-get_genus(self)
-get_species(self)
-get_vulnerability(self)
-get_resilience(self)
-get_status(self)
+- __init__(domain, kingdom, phylum, class_type,name)
+- __str__()
++ set_order(order)
++ set_family(family)
++ set_genus,genus)
++ set_species(species)
++ set_vulnerability(vulnerability)
++ set_resilience(resilience)
++ set_status(status)
++ get_order()
++ get_family()
++ get_genus()
++ get_species()
++ get_vulnerability()
++ get_resilience()
++ get_status()
 
 
 
